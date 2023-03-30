5f0536de1bd6a253c0a5e3caf92560b6ed6551f2
George Bakewell-Smith <georgebs007@gmail.com>
2023-03-30 16:05:58 +0100

    Begin process of making function which initialises a matrix structure.

--- main.c
+++ main.c
@@ -3,19 +3,52 @@
 #include <math.h>
 #include "linAlg.h"
 
+int *createMatrix(size_t a, size_t b);
+
+struct Matrix {
+    int rows;
+    int cols;
+    int **data;
+};
+
+
+
 int main()
 {
-    double a[] = {1,2,3,4,5}, b[] = {5,4,3,2,1};
-    size_t lenArr = sizeof(a) / sizeof(double);
+    int arr[3][3] = {{8,8,90},{4,6,3},{2,6,1}};
+    struct Matrix *m = createMatrix(3,6);
+    m->data[0][0] = 3;
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->data[i][j] = i+j;
+            printf("%d ", m->data[i][j]);
+        }
+        printf("\n");
+    }
+    //struct Matrix *m = createMatrix(3,3);
+    // free the memory allocated for the matrix data
 
-    //add a+b arrays together
-    //double *arr = subArr(a,b,lenArr);
-    double *arr2 = powArrEl(3,a,lenArr);
-    printArr(arr2,lenArr);
+    for (int i = 0; i < m->rows; i++) {
+        free(m->data[i]);
+    }
+    free(m->data);
 
-
-    //freeArray(arr);
-    freeArray(arr2);
+    // free the memory allocated for the Matrix structure
+    free(m);
 
     return 0;
+}
+
+int *createMatrix(size_t m, size_t n){
+    //Allocates memory for a mxn matrix structure, which stores data and the values m,n
+    struct Matrix *mat = (struct Matrix*) malloc(sizeof(struct Matrix));
+    //allocate memory for the matrix data
+    mat->data = (int**) malloc(m * sizeof(int*));
+    for (int i = 0; i < m; i++) {
+        mat->data[i] = (int*) malloc(n * sizeof(int));
+    }
+    mat->rows = m;
+    mat->cols = n;
+
+    return mat;
 }
